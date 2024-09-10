f017ef5761f40857489ec479a42f194543d05915
NicolasPino818 <nicolaspino818@gmail.com>
2024-09-10 14:30:47 -0300

    inicio de proyectos

---
+++ backend_miatraining/src/main/java/com/inovisoft/backend_miatraining/BackendMiatrainingApplication.java
@@ -0,0 +1,13 @@
+package com.inovisoft.backend_miatraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMiatrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMiatrainingApplication.class, args);
+	}
+
+}
