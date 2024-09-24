d67d52a6f849aa798c02f906e660b0566b18558f
Valentina <valerivaas1@gmail.com>
2024-09-24 13:10:28 -0300

    Vista Mi Cuenta

--- portalweb/src/app/views/client/account-settings-view/account-settings-view.component.ts
+++ portalweb/src/app/views/client/account-settings-view/account-settings-view.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { accountOptions } from '../../../models/nav';
 
 @Component({
   selector: 'app-account-settings-view',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './account-settings-view.component.html',
-  styleUrl: './account-settings-view.component.css'
+  styleUrls: ['./account-settings-view.component.css']
 })
 export class AccountSettingsViewComponent {
-
+  accountOptions = accountOptions.clientOptions;
 }
