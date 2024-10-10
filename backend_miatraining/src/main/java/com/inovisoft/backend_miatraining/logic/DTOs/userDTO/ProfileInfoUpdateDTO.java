618f640bca4539034194a0ddc0abeabddea5ae79
NicolasPino818 <nicolaspino818@gmail.com>
2024-10-10 02:29:20 -0300

    Configuracion perfil

--- backend_miatraining/src/main/java/com/inovisoft/backend_miatraining/logic/DTOs/userDTO/ProfileInfoUpdateDTO.java
+++ backend_miatraining/src/main/java/com/inovisoft/backend_miatraining/logic/DTOs/userDTO/ProfileInfoUpdateDTO.java
@@ -1,2 +1,15 @@
-package com.inovisoft.backend_miatraining.logic.DTOs.userDTO;public class ProfileInfoUpdateDTO {
+package com.inovisoft.backend_miatraining.logic.DTOs.userDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProfileInfoUpdateDTO {
+    private String email;
+    private String pictureUrlString;
 }
